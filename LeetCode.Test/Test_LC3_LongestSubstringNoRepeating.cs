eb35f564c543285ac6100d7f9beb6a2c71eb0f8d
micahmeadows <micahstlp@gmail.com>
2022-01-23 22:33:01 -0500

    Add project files.

---
+++ LeetCode.Test/Test_LC3_LongestSubstringNoRepeating.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Problems;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Test
+{
+    [TestFixture]
+    public class Test_LC3_LongestSubstringNoRepeating
+    {
+
+        [TestCase("abcabcbb", ExpectedResult = 3)]
+        [TestCase("bbbbb", ExpectedResult = 1)]
+        [TestCase("pwwkew", ExpectedResult = 3)] 
+        public int LengthOfLongestSubstringTest(string s)
+        {
+            return LC3_LongestSubstringNoRepeating.LengthOfLongestSubstring(s);
+        }
+    }
+}
---
+++ LeetCode/Problems/LC3_LongestSubstringNoRepeating.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public static class LC3_LongestSubstringNoRepeating
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int leftIdx = 0;
+            int longestSubstringLen = 0;
+            HashSet<char> curSubstringLetters = new HashSet<char>();
+
+            for (int rightIdx = 0; rightIdx < s.Length; rightIdx++)
+            {
+                while (curSubstringLetters.Contains(s[rightIdx]))
+                {
+                    curSubstringLetters.Remove(s[leftIdx]);
+                    leftIdx++;
+                }
+                curSubstringLetters.Add(s[rightIdx]);
+                longestSubstringLen = Math.Max(longestSubstringLen, (rightIdx - leftIdx) + 1);
+            }
+
+            return longestSubstringLen;
+        }
+    }
+}
